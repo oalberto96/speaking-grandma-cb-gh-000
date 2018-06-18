70695261a09585e482d0d6f87922b73937492e14
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-18 01:27:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -15,4 +15,8 @@
 def speak_to_grandma(sound)
   if sound == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif sound == sound.uppercase
+    return "NO, NOT SINCE 1938"
+  else
+    return "HUH? SPEAK UP, SONNY!"
 end
